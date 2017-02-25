0d9fd807807a5447e511b0e8d07ac3e5b915512c
chtu <tuttle.charlie@gmail.com>
2017-02-24 20:17:53 -0800

    added homepage and navbar

--- personalwebsite/urls.py
+++ personalwebsite/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from django.conf.urls.static import static
 from django.contrib import admin
+
+from home import views
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'$', views.home, name="home"),
 ]
+
+if settings.DEBUG:
+	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
